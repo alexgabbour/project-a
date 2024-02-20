5faab77be178522442ed86f86d360a721129ae5c
Alex Gabbour <alex.gabbour@gmail.com>
2024-02-20 13:44:57 -0500

    db_config class for all database configuration functions

--- rss-db-interface.py
+++ rss-db-interface.py
@@ -1,3 +1,23 @@
 #!/bin/python3
 
 import sqlite3
+
+conx = sqlite3.connect('cache.db')
+
+cur = conx.cursor()
+
+class db_config():
+    def init_db():
+        cur.execute("CREATE TABLE news (publication text, headline text, author text, date text, content text)")
+
+        conx.commit()
+
+    def add_story(publication, headline, author, date, content):
+        cur.execute("INSERT INTO news VALUES (:publication, :headline, :author, :date,:content)", 
+                    {'publication': publication, 'headline': headline, 'author': author, 'date': date, 'content': content})
+
+        conx.commit()
+
+#db_config.add_story('Washington Post', 'Trump running again?', 'Billy Bob', '4 June, 2020', 'Well it\'s happening again. Who could have seen this coming')
+
+conx.close()
