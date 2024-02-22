1a949f16ff40daa20aaa5b0f051601b5594c245a
Alex Gabbour <alex.gabbour@gmail.com>
2024-02-22 12:29:07 -0500

    new functions

--- db_manager.py
+++ db_manager.py
@@ -13,14 +13,16 @@ class db_config():
             conx = sqlite3.connect('cache.db')
             cur = conx.cursor()
             
-            for cat in ['allnews', 'politics', 'finance', 'sports']:
+            for cat in ['allnews', 'politics', 'finance', 'sports', 'digest', 'settings']:
                 cur.execute(f"CREATE TABLE {cat} (publication text, headline text, author text, date text, content text, category text)")
 
             conx.commit()
             conx.close()
 
+
+#modify database entries
 class db_mod():
-    #add a story entry to the 'allnews' table of the database
+    #add a story entry to the 'allnews' table
     def add_story(publication, headline, author, date, content, category):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
@@ -28,7 +30,9 @@ class db_mod():
                     {'publication': publication, 'headline': headline, 'author': author, 'date': date, 'content': content, 'category': category})
 
         conx.commit()
+        conx.close()
     
+    #add a story to a specific table
     def add_to_table(publication, headline, author, date, content, category, table):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
@@ -36,6 +40,22 @@ class db_mod():
                     {'publication': publication, 'headline': headline, 'author': author, 'date': date, 'content': content, 'category': category})
 
         conx.commit()
+        conx.close()
 
+    #remove a story from allnews
+    def rm_from_table(headline):
+        conx = sqlite3.connect('cache.db')
+        cur = conx.cursor()
+        cur.execute(f"DELETE FROM allnews WHERE headline = :headline", {'headline': headline})
 
-db_config.create_db()
+        conx.commit()
+        conx.close()
+    
+    def cp_story(headline, table1, table2):
+        conx = sqlite3.connect('cache.db')
+        cur = conx.cursor()
+        cur.execute(f"INSERT INTO {table2} (publication, headline, author, date, content, category)
+                    SELECT publication, :headline, author, date, content, category FROM {table1}", {'headline': headline})
+
+        conx.commit()
+        conx.close()
