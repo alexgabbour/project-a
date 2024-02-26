cd4e763ee2f09455494e6b95f6452b1c6ab59d58
Alex Gabbour <alex.gabbour@gmail.com>
2024-02-26 18:06:51 -0500

    shoved the function into a class

--- xml_parser.py
+++ xml_parser.py
@@ -5,36 +5,37 @@
 
 import xml.dom.minidom
 
-#generate a set of nicely formatted stories from a specified RSS feed XML file
-def getStories(xmlFile):
-    domtree = xml.dom.minidom.parse(xmlFile)
-    group = domtree.documentElement
-    stories = group.getElementsByTagName('item')
+class parser():
+    #generate a set of nicely formatted stories from a specified RSS feed XML file
+    def getStories(xmlFile):
+        domtree = xml.dom.minidom.parse(xmlFile)
+        group = domtree.documentElement
+        stories = group.getElementsByTagName('item')
 
-    for story in stories:
-        #check for the existance of tags - replace with nothing if they don't exist
+        for story in stories:
+            #check for the existance of tags - replace with nothing if they don't exist
 
-        try:
-            title = story.getElementsByTagName('title')[0].childNodes[0].nodeValue
-        except Exception:
-            author = '*No title*'
+            try:
+                title = story.getElementsByTagName('title')[0].childNodes[0].nodeValue
+            except Exception:
+                author = '*No title*'
 
-        try:
-            date = story.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue
-        except Exception:
-            author = '*No date*'
+            try:
+                date = story.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue
+            except Exception:
+                author = '*No date*'
 
-        try:
-            author = story.getElementsByTagName('dc:creator')[0].childNodes[0].nodeValue
-        except Exception:
-            author = '*No author*'
-            
-        try:
-            desc = story.getElementsByTagName('media:description')[0].childNodes[0].nodeValue
-        except Exception:
-            desc = '*No description*'
+            try:
+                author = story.getElementsByTagName('dc:creator')[0].childNodes[0].nodeValue
+            except Exception:
+                author = '*No author*'
+                
+            try:
+                desc = story.getElementsByTagName('media:description')[0].childNodes[0].nodeValue
+            except Exception:
+                desc = '*No description*'
 
-        #take the variable names and print them to the terminal - these values will be sent to the databases in the end
-        print(f"--- {title} ---\n Author: {author}\n Date: {date}\n Description: {desc}\n")
+            #take the variable names and print them to the terminal - these values will be sent to the databases in the end
+            print(f"--- {title} ---\n Author: {author}\n Date: {date}\n Description: {desc}\n")
 
-getStories('US.xml')
+parser.getStories('US.xml')
