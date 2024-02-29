f5a62b5b01336b08c0326711a02b2f4909c4819e
Alex Gabbour <alex.gabbour@gmail.com>
2024-02-29 11:25:50 -0500

    variable alignment

--- db_manager.py
+++ db_manager.py
@@ -13,8 +13,8 @@ class db_config():
             conx = sqlite3.connect('cache.db')
             cur = conx.cursor()
             
-            for cat in ['allnews', 'politics', 'finance', 'sports', 'digest', 'settings']:
-                cur.execute(f"CREATE TABLE {cat} (publication text, headline text, author text, date text, content text, category text)")
+            for cat in ['allnews', 'politics', 'finance', 'sports', 'digest']:
+                cur.execute(f"CREATE TABLE {cat} (title text, author text, date text, desc text)")
 
             conx.commit()
             conx.close()
@@ -23,39 +23,40 @@ class db_config():
 #modify database entries
 class db_mod():
     #add a story entry to the 'allnews' table
-    def add_story(publication, headline, author, date, content, category):
+    def add_story(title, author, date, desc):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
-        cur.execute("INSERT INTO allnews VALUES (:publication, :headline, :author, :date, :content, :category)", 
-                    {'publication': publication, 'headline': headline, 'author': author, 'date': date, 'content': content, 'category': category})
+        cur.execute("INSERT INTO allnews VALUES (:title, :author, :date, :desc)", 
+                    {'title': title, 'author': author, 'date': date, 'desc': desc})
 
         conx.commit()
         conx.close()
     
     #add a story to a specific table
-    def add_to_table(publication, headline, author, date, content, category, table):
+    def add_to_table(title, author, date, desc, table):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
-        cur.execute(f"INSERT INTO {table} VALUES (:publication, :headline, :author, :date, :content, :category)", 
-                    {'publication': publication, 'headline': headline, 'author': author, 'date': date, 'content': content, 'category': category})
+        cur.execute(f"INSERT INTO {table} VALUES (:title, :author, :date, :desc)", 
+                    {'title': title, 'author': author, 'date': date, 'desc': desc})
 
         conx.commit()
         conx.close()
 
     #remove a story from allnews
-    def rm_from_table(headline):
+    def rm_from_table(title):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
-        cur.execute(f"DELETE FROM allnews WHERE headline = :headline", {'headline': headline})
+        cur.execute(f"DELETE FROM allnews WHERE title = :title", {'title': title})
 
         conx.commit()
         conx.close()
     
-    def cp_story(headline, table1, table2):
+    def cp_story(title, table1, table2):
         conx = sqlite3.connect('cache.db')
         cur = conx.cursor()
-        cur.execute(f"INSERT INTO {table2} (publication, headline, author, date, content, category)
-                    SELECT publication, :headline, author, date, content, category FROM {table1}", {'headline': headline})
+        cur.execute(f"INSERT INTO {table2} (headline, author, date, desc) SELECT :title, author, date, desc FROM {table1}", {'title': title})
 
         conx.commit()
         conx.close()
+
+db_config.create_db()
--- xml_parser.py
+++ xml_parser.py
@@ -1,41 +1,53 @@
 #!/bin/python3
 
 ## pick out the categories
-## create a story object (??) for the db manager to sort into the proper table
+## create a story object (?? dictionnaries could come in handy here) for the db manager to sort into the proper table
+## this is the core of the parser only. it'll get uncategorized stories into a database, nicely formatted.
+## getStories function will have to be given or determine a publication as an input so it can tag its stories  IN THE FUTURE!
+## for v1.0 getStories will just have to differentiate between a few xml files.
 
 import xml.dom.minidom
+import sqlite3
 
 class parser():
     #generate a set of nicely formatted stories from a specified RSS feed XML file
+
     def getStories(xmlFile):
         domtree = xml.dom.minidom.parse(xmlFile)
         group = domtree.documentElement
         stories = group.getElementsByTagName('item')
 
+        storyBlock = {}
+        id = 0
         for story in stories:
             #check for the existance of tags - replace with nothing if they don't exist
 
             try:
                 title = story.getElementsByTagName('title')[0].childNodes[0].nodeValue
-            except Exception:
+            except IndexError:
                 author = '*No title*'
 
             try:
                 date = story.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue
-            except Exception:
+            except IndexError:
                 author = '*No date*'
 
             try:
                 author = story.getElementsByTagName('dc:creator')[0].childNodes[0].nodeValue
-            except Exception:
+            except IndexError:
                 author = '*No author*'
                 
             try:
                 desc = story.getElementsByTagName('media:description')[0].childNodes[0].nodeValue
-            except Exception:
+            except IndexError:
                 desc = '*No description*'
 
-            #take the variable names and print them to the terminal - these values will be sent to the databases in the end
-            print(f"--- {title} ---\n Author: {author}\n Date: {date}\n Description: {desc}\n")
+            #after each story is parsed, store it in a dictionnary as a tuple for immutability
+            storyBlock[id] = (title, date, author, desc)
+            id = id + 1
+
+        print(storyBlock)
+        
+
 
 parser.getStories('US.xml')
